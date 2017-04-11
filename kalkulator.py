ee49af10d69c02d07fcc74648115b6992104d10a
Hanna Mikołajczyk <34386@student.ue.poznan.pl>
2017-04-11 19:15:45 +0200

    rozgrzebana_funkcja_dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,5 @@
-def dodawanie():
-    a = int(input('Podaj pierwsza liczbe'))
-    b = int(input('Podaj pierwsza liczbe'))
-    print(a+b)
-    
+def dodawanie(a, b):
+     wynik = a +b
+
 dodawanie()
    
