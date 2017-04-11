aa18e0a83c1cd942ae5cb73bfd73003228d279ba
Hanna Mikołajczyk <34386@student.ue.poznan.pl>
2017-04-11 19:25:35 +0200

    funkcja z pomocą gotowa do review

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,8 @@
-print('Hello world')
+def dodawanie(a, b):
+     a = int(input('podaj pierwsza liczbe'))
+     b = int(input('podaj pierwsza liczbe'))
+     
+def get_help():
+  print('Tojest prosty program')
+
+dodawanie()
